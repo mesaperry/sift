6d8a7b3cd849d987406a0c943554d5f234eb307a
Mesa <mesperrus@gmail.com>
2021-05-11 18:48:50 -0500

    clean up benchmark

--- benchmark.cpp
+++ benchmark.cpp
@@ -19,13 +19,6 @@ openCVSIFTDetect( const cv::Mat image )
 
     cv::Mat descriptors;
     sift.compute(image, keypoints, descriptors);
-
-    // render and output image
-    // cv::Mat output;
-    // cv::drawKeypoints(image, keypoints, output);
-    // cv::imwrite("opencv_res.jpg", output);
-
-    // return descriptors;
 }
 
 void
@@ -41,15 +34,7 @@ meSIFTDetect( const cv::Mat image )
     meSIFT(image, cv::noArray(), keypoints, descriptors, true);
     printf( "Time to run meSIFT for descriptors: %0.2f\n",
             ((float)clock() - start) / CLOCKS_PER_SEC );
-
-    // render and output image
-    // cv::Mat output;
-    // cv::drawKeypoints(image, keypoints, output);
-    // cv::imwrite("me_res.jpg", output);
-
-    // return descriptors
 }
-
 
 int
 main( int argc, const char* argv[] )
@@ -67,25 +52,6 @@ main( int argc, const char* argv[] )
     meSIFTDetect(input_img);
     printf( "Time to compute meSIFT: %0.2f\n",
             ((float)clock() - start2) / CLOCKS_PER_SEC );
-
-    // cv::BFMatcher matcher;
-    // std::vector<cv::DMatch> matches;
-    // matcher.match(desc1, desc2, matches);
-    // cv::drawMatches();
-
-
-// // Draws matches of keypints from two images on output image.
-// CV_EXPORTS void drawMatches( const Mat& img1, const vector<KeyPoint>& keypoints1,
-//                              const Mat& img2, const vector<KeyPoint>& keypoints2,
-//                              const vector<DMatch>& matches1to2, Mat& outImg,
-//                              const Scalar& matchColor=Scalar::all(-1), const Scalar& singlePointColor=Scalar::all(-1),
-//                              const vector<char>& matchesMask=vector<char>(), int flags=DrawMatchesFlags::DEFAULT );
-
-// CV_EXPORTS void drawMatches( const Mat& img1, const vector<KeyPoint>& keypoints1,
-//                              const Mat& img2, const vector<KeyPoint>& keypoints2,
-//                              const vector<vector<DMatch> >& matches1to2, Mat& outImg,
-//                              const Scalar& matchColor=Scalar::all(-1), const Scalar& singlePointColor=Scalar::all(-1),
-//                              const vector<vector<char> >& matchesMask=vector<vector<char> >(), int flags=DrawMatchesFlags::DEFAULT );
 
     return 0;
 }
